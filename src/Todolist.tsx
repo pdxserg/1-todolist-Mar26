f6aa2734bc87fd7737a60e4cd3490cb31ac3da27
Sergey Starchenko <pdxserg@gmail.com>
2024-03-27 12:55:02 -0700

    start from 0

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,30 +1,25 @@
 import React from "react";
 
-
-type TodolistPropsType = {
-	title?: string,
-}
-export function Todolist(props:TodolistPropsType) {
-
+export function Todolist() {
 	return (
-		<div>
-			<h3>{props.}/h3>
-			<div>
-				<input type="text"/>
-				<button>+</button>
+		<div className="App">
+			<div className="todolist">
+				<h3>What to learn</h3>
+				<div>
+					<input/>
+					<button>+</button>
+				</div>
+				<ul>
+					<li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+					<li><input type="checkbox" checked={true}/> <span>JS</span></li>
+					<li><input type="checkbox" checked={false}/> <span>React</span></li>
+				</ul>
+				<div>
+					<button>All</button>
+					<button>Active</button>
+					<button>Completed</button>
+				</div>
 			</div>
-			<ul>
-				<li><input type="checkbox" checked={true}/><span>HTML</span></li>
-				<li><input type="checkbox" checked={true}/><span>JS</span></li>
-				<li><input type="checkbox" checked={false}/><span>REACT</span></li>
-
-			</ul>
-			<div>
-				<button>All</button>
-				<button>Action</button>
-				<button>Completed</button>
-			</div>
-
 		</div>
 	)
 }
