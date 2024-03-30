a71e39f5279e584da936e439b139b978c47167c5
SergeyStarchenko <pdxserg@gmail.com>
2024-03-29 19:24:37 -0700

    repeat

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import './App.css';
-import {TasksPropsType, Todolist} from "./Todolist";
+import { Todolist} from "./Todolist";
 
 
 
 
 
 function App() {
-	 let task1: Array<TasksPropsType>  = [
-		{id: 1, lesson: "CSS", isDone: true},
-		{id: 2, lesson: "HTML", isDone: false},
-		{id: 3, lesson: "REACT", isDone: true},
-	]
-	 let task2 = [
-		{id: 1, lesson: "a", isDone: false},
-		{id: 2, lesson: "b", isDone: true},
-		{id: 3, lesson: "c", isDone: true},
-		{id: 4, lesson: "d", isDone: true},
-	]
-	const todolist1 = "What to learn"
+
 	return (
 		<div>
-			<Todolist title = {todolist1} tasks={task1} />
-			<Todolist title = "What to do"  tasks={task2}/>
+			<div>
+				<Todolist />
+				<Todolist />
 
+			</div>
 		</div>
 
 
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,45 +2,22 @@ import React from "react";
 
 
 
-type TodolistPropsType = {
-	title: string
-	 tasks: Array<TasksPropsType>
-}
-export type TasksPropsType = {
-	 id: number
-	 lesson: string
-	 isDone: boolean
-}
 
-export function Todolist({tasks, title}: TodolistPropsType,  ) {
-	// const type = props.title
-	// const tasks = props.tasks
-
-	const listItems = []
-	for(let i =0; i < tasks.length; i++ ){
-		listItems.push(
-			<li>
-			 	<input type="checkbox"
-			 	           checked={tasks[i].isDone}/>
-			 		<span>{tasks[i].lesson}</span>
-			 </li>
-		)
-
-	}
+export function Todolist() {
 
 	return (
 		<div className="App">
 			<div className="todolist">
-				<h3>{title}</h3>
+				<h3>wat lern</h3>
 				<div>
 					<input/>
 					<button>+</button>
 				</div>
 				<ul>
+					<li><input type="checkbox" checked={false}/> <span>ddd</span></li>
+					<li><input type="checkbox" checked={false}/> <span>ddd</span></li>
+					<li><input type="checkbox" checked={false}/> <span>ddd</span></li>
 
-					{/*<li><input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].lesson}</span></li>*/}
-					{/*<li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].lesson}</span></li>*/}
-					{/*<li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].lesson}</span></li>*/}
 
 				</ul>
 				<div>
@@ -52,3 +29,12 @@ export function Todolist({tasks, title}: TodolistPropsType,  ) {
 		</div>
 	)
 }
+
+
+
+
+
+
+
+
+
