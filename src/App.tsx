326ed500e96ee653e7d3fc9be2c25bec87b0bb91
SergeyStarchenko <pdxserg@gmail.com>
2024-03-29 21:05:53 -0700

    map begin

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,22 @@ import { Todolist} from "./Todolist";
 
 function App() {
 
+	let task1 = [
+		{someItem: "html", isDone: true},
+		{someItem: "css", isDone: true},
+		{someItem: "reakt", isDone: false},
+	]
+	let task2 = [
+		{someItem: "milk", isDone: true},
+		{someItem: "ice", isDone: true},
+		{someItem: "bear", isDone: false},
+		{someItem: "smoke", isDone: false},
+	]
 	return (
 		<div>
 			<div>
-				<Todolist />
-				<Todolist />
+				<Todolist title = "What to  learn" tasks={task1}  />
+				<Todolist title = "What to  eat" tasks={task2}/>
 
 			</div>
 		</div>
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 
+type TasksPropsType={
+	someItem: string;
+	isDone: boolean
+}
 
+type TodolistPropsType ={
+	title: string;
+	tasks: Array<TasksPropsType>;
+}
 
+export function Todolist(props: TodolistPropsType) {
+const nameCard = props.title
+	const nameSomeitem = props.tasks
 
-export function Todolist() {
 
 	return (
 		<div className="App">
 			<div className="todolist">
-				<h3>wat lern</h3>
+				<h3>wat {nameCard}</h3>
 				<div>
 					<input/>
 					<button>+</button>
 				</div>
 				<ul>
-					<li><input type="checkbox" checked={false}/> <span>ddd</span></li>
-					<li><input type="checkbox" checked={false}/> <span>ddd</span></li>
-					<li><input type="checkbox" checked={false}/> <span>ddd</span></li>
+					<li><input type="checkbox" checked={nameSomeitem[0].isDone}/> <span>{nameSomeitem[0].someItem}</span></li>
+					<li><input type="checkbox" checked={nameSomeitem[1].isDone}/> <span>{nameSomeitem[1].someItem}</span></li>
+					<li><input type="checkbox" checked={nameSomeitem[2].isDone}/> <span>{nameSomeitem[2].someItem}</span></li>
+
+
 
 
 				</ul>
